97580c1f259bd050e753cc5d98225a032d80abab
Raphael Wildhaber <wildhaber@godesign.ch>
2016-06-29 17:19:26 +0200

    sent TrackyOptions to separate file

---
+++ src/tracky.options.js
@@ -0,0 +1,14 @@
+export default {
+  classPrefix: 'tracky-',
+  classSuffix: '',
+  classGtPrefix: 'gt-',
+  classLtPrefix: 'lt-',
+  classEqPrefix: 'eq-',
+  classBtPrefix: 'bt-',
+  events: {
+    scroll: {
+      enabled: true,
+      breakpoints: []
+    }
+  }
+}
