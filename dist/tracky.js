80999ae3baf8921cc8f8945914f0656b5a385f4e
Raphael Wildhaber <wildhaber@godesign.ch>
2016-06-29 21:37:52 +0200

    update dist

--- dist/tracky.js
+++ dist/tracky.js
@@ -708,6 +708,170 @@
 	        }
 	      });
 	    }
+	
+	    /**
+	     * _buildClassName
+	     * @param value
+	     * @param modifier
+	     * @returns {string}
+	     * @private
+	     */
+	
+	  }, {
+	    key: '_buildClassName',
+	    value: function _buildClassName(value, modifier) {
+	      var o = this._globalOptions;
+	      return o.classPrefix + this._key + '-' + modifier + value + o.classSuffix;
+	    }
+	
+	    /**
+	     * _transformValue
+	     * @param value
+	     * @returns {*}
+	     * @private
+	     */
+	
+	  }, {
+	    key: '_transformValue',
+	    value: function _transformValue(value) {
+	
+	      var t = null;
+	
+	      if (typeof value === 'number') {
+	        t = [value, { percent: false }];
+	      } else if (typeof value === 'string') {
+	        t = value.indexOf('%') == value.length - 1 ? [parseInt(value, 10), { percent: true }] : null;
+	      } else {
+	        t = value;
+	      }
+	
+	      return t;
+	    }
+	
+	    /**
+	     * _transformBreakpoints
+	     * @param bp
+	     * @returns {Array|*}
+	     * @private
+	     */
+	
+	  }, {
+	    key: '_transformBreakpoints',
+	    value: function _transformBreakpoints(bp) {
+	      var _this7 = this;
+	
+	      return bp.map(function (p) {
+	
+	        var go = _this7._globalOptions;
+	
+	        var prep = typeof p === 'number' || typeof p === 'string' ? {
+	          value: p
+	        } : p;
+	
+	        var hasBetween = !(typeof prep.min !== 'undefined' && typeof prep.max !== 'undefined') ? false : typeof prep.applyBt !== 'undefined' ? prep.applyBt : true;
+	
+	        var hasValue = !hasBetween && typeof prep.value !== 'undefined';
+	
+	        var hasCustomCss = typeof prep.css !== 'undefined' && !!prep.css;
+	
+	        if (hasCustomCss && typeof prep.css === 'string') {
+	          prep.css = {
+	            eq: prep.css,
+	            bt: prep.css
+	          };
+	        }
+	
+	        if (hasValue) {
+	          prep.value = _this7._transformValue(prep.value);
+	        } else if (hasBetween) {
+	          prep.min = _this7._transformValue(prep.min);
+	          prep.max = _this7._transformValue(prep.max);
+	        }
+	
+	        return {
+	          css: {
+	            lt: !hasBetween ? hasCustomCss && typeof prep.css.lt !== 'undefined' ? prep.css.lt : _this7._buildClassName(prep.value[0] + (prep.value[1].percent ? 'pc' : ''), go.classLtPrefix) : null,
+	            gt: !hasBetween ? hasCustomCss && typeof prep.css.gt !== 'undefined' ? prep.css.gt : _this7._buildClassName(prep.value[0] + (prep.value[1].percent ? 'pc' : ''), go.classGtPrefix) : null,
+	            eq: !hasBetween ? hasCustomCss && typeof prep.css.eq !== 'undefined' ? prep.css.eq : _this7._buildClassName(prep.value[0] + (prep.value[1].percent ? 'pc' : ''), go.classEqPrefix) : null,
+	            bt: hasBetween ? hasCustomCss && typeof prep.css.bt !== 'undefined' ? prep.css.bt : _this7._buildClassName(prep.min[0] + (prep.min[1].percent ? 'pc' : '') + '-' + prep.max[0] + (prep.max[1].percent ? 'pc' : ''), go.classBtPrefix) : null
+	          },
+	          applyLt: !hasBetween && typeof prep.applyLt !== 'undefined' ? prep.applyLt : !hasBetween,
+	          applyGt: !hasBetween && typeof prep.applyGt !== 'undefined' ? prep.applyGt : !hasBetween,
+	          applyEq: !hasBetween && typeof prep.applyEq !== 'undefined' ? prep.applyEq : !hasBetween,
+	          applyBt: hasBetween,
+	          value: hasValue ? prep.value : null,
+	          min: hasBetween ? prep.min : null,
+	          max: hasBetween ? prep.max : null
+	        };
+	      });
+	    }
+	
+	    /**
+	     * _extractClasses
+	     * @returns {Array}
+	     * @private
+	     */
+	
+	  }, {
+	    key: '_extractClasses',
+	    value: function _extractClasses() {
+	      var bps = this._options.breakpoints;
+	      var classArray = [];
+	
+	      bps.forEach(function (bp) {
+	        for (var c in bp.css) {
+	          if (bp.css[c]) {
+	            classArray.push(bp.css[c]);
+	          }
+	        }
+	      });
+	
+	      return classArray;
+	    }
+	
+	    /**
+	     * classify
+	     * @param domNode
+	     * @param value
+	     */
+	
+	  }, {
+	    key: 'classify',
+	    value: function classify(domNode) {
+	      var value = arguments.length <= 1 || arguments[1] === undefined ? { absolute: 0, percent: 0 } : arguments[1];
+	
+	
+	      var bp = this._options.breakpoints;
+	      var classes = [];
+	
+	      if (bp.length > 0) {
+	        for (var l = bp.length; l; l--) {
+	          var _bp = bp[l - 1];
+	
+	          // Check lt
+	          if (_bp.applyLt && (!_bp.value[1].percent && value.absolute < _bp.value[0] || _bp.value[1].percent && value.percent < _bp.value[0])) {
+	            classes.push(_bp.css.lt);
+	          }
+	
+	          // Check gt
+	          if (_bp.applyGt && (!_bp.value[1].percent && value.absolute > _bp.value[0] || _bp.value[1].percent && value.percent > _bp.value[0])) {
+	            classes.push(_bp.css.gt);
+	          }
+	
+	          // Check eq
+	          if (_bp.applyEq && (!_bp.value[1].percent && value.absolute === _bp.value[0] || _bp.value[1].percent && value.percent === _bp.value[0])) {
+	            classes.push(_bp.css.eq);
+	          }
+	
+	          // Check between
+	          if (_bp.applyBt && (!_bp.min[1].percent && value.absolute >= _bp.min[0] || _bp.min[1].percent && value.percent >= _bp.min[0]) && (!_bp.max[1].percent && value.absolute <= _bp.max[0] || _bp.max[1].percent && value.percent <= _bp.max[0])) {
+	            classes.push(_bp.css.bt);
+	          }
+	        }
+	      }
+	
+	      this.attachClasses(domNode, classes);
+	    }
 	  }]);
 	
 	  return TrackyScroll;
@@ -809,170 +973,6 @@
 	    key: 'remove',
 	    value: function remove(nodes) {
 	      this.onRemove(nodes);
-	    }
-	
-	    /**
-	     * _buildClassName
-	     * @param value
-	     * @param modifier
-	     * @returns {string}
-	     * @private
-	     */
-	
-	  }, {
-	    key: '_buildClassName',
-	    value: function _buildClassName(value, modifier) {
-	      var o = this._globalOptions;
-	      return o.classPrefix + this._key + '-' + modifier + value + o.classSuffix;
-	    }
-	
-	    /**
-	     * _transformValue
-	     * @param value
-	     * @returns {*}
-	     * @private
-	     */
-	
-	  }, {
-	    key: '_transformValue',
-	    value: function _transformValue(value) {
-	
-	      var t = null;
-	
-	      if (typeof value === 'number') {
-	        t = [value, { percent: false }];
-	      } else if (typeof value === 'string') {
-	        t = value.indexOf('%') == value.length - 1 ? [parseInt(value, 10), { percent: true }] : null;
-	      } else {
-	        t = value;
-	      }
-	
-	      return t;
-	    }
-	
-	    /**
-	     * _transformBreakpoints
-	     * @param bp
-	     * @returns {Array|*}
-	     * @private
-	     */
-	
-	  }, {
-	    key: '_transformBreakpoints',
-	    value: function _transformBreakpoints(bp) {
-	      var _this = this;
-	
-	      return bp.map(function (p) {
-	
-	        var go = _this._globalOptions;
-	
-	        var prep = typeof p === 'number' || typeof p === 'string' ? {
-	          value: p
-	        } : p;
-	
-	        var hasBetween = !(typeof prep.min !== 'undefined' && typeof prep.max !== 'undefined') ? false : typeof prep.applyBt !== 'undefined' ? prep.applyBt : true;
-	
-	        var hasValue = !hasBetween && typeof prep.value !== 'undefined';
-	
-	        var hasCustomCss = typeof prep.css !== 'undefined' && !!prep.css;
-	
-	        if (hasCustomCss && typeof prep.css === 'string') {
-	          prep.css = {
-	            eq: prep.css,
-	            bt: prep.css
-	          };
-	        }
-	
-	        if (hasValue) {
-	          prep.value = _this._transformValue(prep.value);
-	        } else if (hasBetween) {
-	          prep.min = _this._transformValue(prep.min);
-	          prep.max = _this._transformValue(prep.max);
-	        }
-	
-	        return {
-	          css: {
-	            lt: !hasBetween ? hasCustomCss && typeof prep.css.lt !== 'undefined' ? prep.css.lt : _this._buildClassName(prep.value[0] + (prep.value[1].percent ? 'pc' : ''), go.classLtPrefix) : null,
-	            gt: !hasBetween ? hasCustomCss && typeof prep.css.gt !== 'undefined' ? prep.css.gt : _this._buildClassName(prep.value[0] + (prep.value[1].percent ? 'pc' : ''), go.classGtPrefix) : null,
-	            eq: !hasBetween ? hasCustomCss && typeof prep.css.eq !== 'undefined' ? prep.css.eq : _this._buildClassName(prep.value[0] + (prep.value[1].percent ? 'pc' : ''), go.classEqPrefix) : null,
-	            bt: hasBetween ? hasCustomCss && typeof prep.css.bt !== 'undefined' ? prep.css.bt : _this._buildClassName(prep.min[0] + (prep.min[1].percent ? 'pc' : '') + '-' + prep.max[0] + (prep.max[1].percent ? 'pc' : ''), go.classBtPrefix) : null
-	          },
-	          applyLt: !hasBetween && typeof prep.applyLt !== 'undefined' ? prep.applyLt : !hasBetween,
-	          applyGt: !hasBetween && typeof prep.applyGt !== 'undefined' ? prep.applyGt : !hasBetween,
-	          applyEq: !hasBetween && typeof prep.applyEq !== 'undefined' ? prep.applyEq : !hasBetween,
-	          applyBt: hasBetween,
-	          value: hasValue ? prep.value : null,
-	          min: hasBetween ? prep.min : null,
-	          max: hasBetween ? prep.max : null
-	        };
-	      });
-	    }
-	
-	    /**
-	     * _extractClasses
-	     * @returns {Array}
-	     * @private
-	     */
-	
-	  }, {
-	    key: '_extractClasses',
-	    value: function _extractClasses() {
-	      var bps = this._options.breakpoints;
-	      var classArray = [];
-	
-	      bps.forEach(function (bp) {
-	        for (var c in bp.css) {
-	          if (bp.css[c]) {
-	            classArray.push(bp.css[c]);
-	          }
-	        }
-	      });
-	
-	      return classArray;
-	    }
-	
-	    /**
-	     * classify
-	     * @param domNode
-	     * @param value
-	     */
-	
-	  }, {
-	    key: 'classify',
-	    value: function classify(domNode) {
-	      var value = arguments.length <= 1 || arguments[1] === undefined ? { absolute: 0, percent: 0 } : arguments[1];
-	
-	
-	      var bp = this._options.breakpoints;
-	      var classes = [];
-	
-	      if (bp.length > 0) {
-	        for (var l = bp.length; l; l--) {
-	          var _bp = bp[l - 1];
-	
-	          // Check lt
-	          if (_bp.applyLt && (!_bp.value[1].percent && value.absolute < _bp.value[0] || _bp.value[1].percent && value.percent < _bp.value[0])) {
-	            classes.push(_bp.css.lt);
-	          }
-	
-	          // Check gt
-	          if (_bp.applyGt && (!_bp.value[1].percent && value.absolute > _bp.value[0] || _bp.value[1].percent && value.percent > _bp.value[0])) {
-	            classes.push(_bp.css.gt);
-	          }
-	
-	          // Check eq
-	          if (_bp.applyEq && (!_bp.value[1].percent && value.absolute === _bp.value[0] || _bp.value[1].percent && value.percent === _bp.value[0])) {
-	            classes.push(_bp.css.eq);
-	          }
-	
-	          // Check between
-	          if (_bp.applyBt && (!_bp.min[1].percent && value.absolute >= _bp.min[0] || _bp.min[1].percent && value.percent >= _bp.min[0]) && (!_bp.max[1].percent && value.absolute <= _bp.max[0] || _bp.max[1].percent && value.percent <= _bp.max[0])) {
-	            classes.push(_bp.css.bt);
-	          }
-	        }
-	      }
-	
-	      this.attachClasses(domNode, classes);
 	    }
 	
 	    /**
